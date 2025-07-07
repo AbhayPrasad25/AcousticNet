08b6944246224e4c391d255e7b1c9359e89b7b56
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-07 17:00:46 +0530

    Training file

--- train.py
+++ train.py
@@ -2,11 +2,17 @@ import sys
 import pandas as pd
 import modal
 from pathlib import Path
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torch
 import torchaudio
 import torch.nn as nn
 import torchaudio.transforms as T
+
+from model import AudioCNN
+from torch.optim.lr_scheduler import OneCycleLR
+import torch.optim as optim
+from tqdm import tqdm
+import numpy as np
 app = modal.App("audio-cnn")
 
 image = (modal.Image.debian_slim()
@@ -60,6 +66,24 @@ class ESC50Dataset(Dataset):
             spectogram = waveform
         
         return spectogram, row['label']
+    
+def mixup_data(x,y):
+    lam = np.random.beta(0.2,0.2)
+
+    batch_size = x.size(0)
+    index = torch.randperm(batch_size).to(x.device)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a,y_b = y, y[index]
+
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1-lam) * criterion(pred, y_b)
+
+
+
 
 @app.function(image=image, gpu="A10G", volumes={"/data": volume, "/models": model_volume}, timeout=60 * 60 * 3)
 def train():
@@ -98,6 +122,99 @@ def train():
     
     print(f"Training Samples : {len(train_dataset)}")
     print(f"Val Samples : {len(val_dataset)}")
+
+    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+    device = torch.device('cuda'  if torch.cuda.is_available() else 'cpu')
+    model = AudioCNN(num_classes=len(train_dataset.classes))
+    model.to(device)
+
+    num_epochs = 100
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
+    optimizer = optim.AdamW(model.parameters(), lr=0.005, weight_decay=0.01)
+
+    scheduler = OneCycleLR(
+        optimizer,
+        max_lr=0.002,
+        epochs=num_epochs,
+        steps_per_epoch=len(train_dataloader),
+        pct_start=0.1
+    )
+
+    best_accuracy = 0.0
+
+    print("Starting training")
+    for epoch in range(num_epochs):
+        model.train()
+        epoch_loss = 0.0
+
+        progress_bar = tqdm(train_dataloader, desc=f'Epocj {epoch+1}/{num_epochs}')
+        for data, target in progress_bar:
+            data, target = data.to(device), target.to(device)
+
+            ## data mixing 
+            if np.random.random() > 0.7:
+                data, target_a, target_b, lam = mixup_data(data, target)
+                output = model(data)
+                loss = mixup_criterion(
+                    criterion,
+                    output,
+                    target_a,
+                    target_b,
+                    lam
+                )
+            else:
+                output = model(data)
+                loss = criterion(output, target)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            epoch_loss += loss.item()
+            progress_bar.set_postfix({'Loss': f'{loss.item():.4f}'})
+
+        avg_epoch_loss = epoch_loss / len(train_dataloader)
+
+        # Validation after each epoch
+        model.eval()      
+
+        correct = 0
+        total = 0
+        val_loss = 0
+
+        with torch.no_grad():
+            for data, target in test_dataloader:
+                data, target = data.to(device), target.to(device)
+                outputs = model(data)
+                loss = criterion(data)
+                val_loss += loss.item()
+
+                _, predicted = torch.max(outputs.data, 1)
+                total += target.size(0)
+                correct += (predicted == target).sum().item()
+
+        accuracy = 100 * correct / total
+        avg_val_loss = val_loss / len(test_dataloader)
+
+        print(
+            f'Epoch {epoch + 1} Loss: {avg_epoch_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Accuracy: {accuracy:.2f}%')
+        
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'accuracy': accuracy,
+                'epoch': epoch,
+                'classes': train_dataset.classes
+            }, '/models/best_model.pth')
+
+            print('New best model Saved : {accuracy:.2f}%')
+
+    print(f'Training Completed! Best accuracy: {best_accuracy:.2f}%')
+
 
 @app.local_entrypoint()
 def main():
